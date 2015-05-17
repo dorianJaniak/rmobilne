850e9db8cb76b293e062961bce5c172118c85ab3
Dorian Janiak <dorianjaniak@wp.pl>
2015-05-17 23:21:14 +0200

    Dodana obsluga modulu Bluetooth

--- programNUCLEO/main.cpp
+++ programNUCLEO/main.cpp
@@ -1,12 +1,15 @@
 #include "mbed.h"
 
 Ticker silnikKrokTimer;
+Ticker bUARTTimer;
 
 DigitalOut led1(LED1);
 DigitalOut sKrok1(PC_6);
 DigitalOut sKrok2(PC_7);
 DigitalOut sKrok3(PC_8);
 DigitalOut sKrok4(PC_9);
+Serial bUART(PA_9,PA_10);  //Bluetooth UART(TX,RX)
+
 
 const double sKrokKat = 0.087890625;        // Ile stopni / krok silnika
 
@@ -18,7 +21,9 @@ bool sKrokSterowanie [][4] = {  {1, 0, 0, 1},
                                 {0, 0, 1, 0},
                                 {0, 0, 1, 1},
                                 {0, 0, 0, 1} };
-                                
+       
+        
+                         
 void sKrokObroc(double kat, double vKatNaS)
 {
     led1 = led1 | true;
@@ -44,16 +49,41 @@ void sKrokObroc(double kat, double vKatNaS)
 }
 
 void toggle_led() {
-    //led1 = !led1;
     sKrokObroc(10.0, 8);
+}
+
+void bUARTReakcja()
+{   
+    while(bUART.readable())
+    {
+        uint8_t znak = bUART.getc();
+        if(znak == 'a')
+            sKrokObroc(10.0,5);
+        else if(znak == 'b')
+            sKrokObroc(10.0,30);
+        bUART.putc(znak);  
+    }
 }
 
 
 
+void inicjalizuj()
+{
+    bUART.baud(9600);
+    bUART.format();
+    wait_ms(5000);
+    bUART.printf("Jestem Robotem mobilnym Doriana i Marcina\r\nGotow do pracy!\r\n");
+    bUART.attach(&bUARTReakcja, Serial::RxIrq);
+   // silnikKrokTimer.attach(&toggle_led, 2);
+    
+}      
+
 int main() {
-    // Init the ticker
-    silnikKrokTimer.attach(&toggle_led, 2);
+    inicjalizuj();
     while (true) {
-        
+        led1 = true;
+        wait_ms(1000);
+        led1 = false;
+        wait_ms(1000);
     }
 }
